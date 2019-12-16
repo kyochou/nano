5e76d514bc7db830c52f1c3b1e5798dc11043cf4
Kyo Chou <develop.kyo@gmail.com>
2019-12-16 09:46:43 +0800

    client api

---
+++ client/main.go
@@ -0,0 +1,321 @@
+package client
+
+import (
+	"encoding/json"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/gorilla/websocket"
+	"github.com/lonng/nano/internal/codec"
+	"github.com/lonng/nano/internal/message"
+	"github.com/lonng/nano/internal/packet"
+)
+
+var (
+	hsd []byte // handshake data
+	had []byte // handshake ack data
+	hbd []byte
+)
+
+func init() {
+	var err error
+	hsd, err = codec.Encode(packet.Handshake, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	had, err = codec.Encode(packet.HandshakeAck, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	hbd, err = codec.Encode(packet.Heartbeat, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type (
+
+	// Callback represents the callback type which will be called
+	// when the correspond events is occurred.
+	Callback func(data []byte)
+
+	// Connector is a tiny Nano client
+	Connector struct {
+		conn        *websocket.Conn // low-level connection
+		codec       *codec.Decoder  // decoder
+		die         chan struct{}   // connector close channel
+		chSend      chan []byte     // send queue
+		mid         uint            // message id
+		heartTicker *time.Ticker
+
+		// events handler
+		muEvents sync.RWMutex
+		events   map[string]Callback
+
+		// response handler
+		muResponses sync.RWMutex
+		responses   map[uint]Callback
+
+		connectedCallback func() // connected callback
+	}
+)
+
+// NewConnector create a new Connector
+func NewConnector() *Connector {
+	return &Connector{
+		die:       make(chan struct{}),
+		codec:     codec.NewDecoder(),
+		chSend:    make(chan []byte, 64),
+		mid:       1,
+		events:    map[string]Callback{},
+		responses: map[uint]Callback{},
+	}
+}
+
+// Start connect to the server and send/recv between the c/s
+func (c *Connector) Start(addr string) error {
+	// log.Printf("connecting to %s", addr)
+	wsConn, _, err := websocket.DefaultDialer.Dial(addr, nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+
+	c.conn = wsConn
+
+	go c.write()
+
+	// send handshake packet
+	c.send(hsd)
+
+	// read and process network message
+	go c.read()
+
+	return nil
+}
+
+// OnConnected set the callback which will be called when the client connected to the server
+func (c *Connector) OnConnected(callback func()) {
+	c.connectedCallback = callback
+}
+
+// Request send a request to server and register a callbck for the response
+func (c *Connector) Request(route string, v proto.Message, callback Callback) error {
+	data, err := serialize(v)
+	if err != nil {
+		return err
+	}
+
+	msg := &message.Message{
+		Type:  message.Request,
+		Route: route,
+		ID:    c.mid,
+		Data:  data,
+	}
+
+	c.setResponseHandler(c.mid, callback)
+	if err := c.sendMessage(msg); err != nil {
+		c.setResponseHandler(c.mid, nil)
+		return err
+	}
+
+	return nil
+}
+
+// Notify send a notification to server
+func (c *Connector) Notify(route string, v proto.Message) error {
+	data, err := serialize(v)
+	if err != nil {
+		return err
+	}
+
+	msg := &message.Message{
+		Type:  message.Notify,
+		Route: route,
+		Data:  data,
+	}
+	// log.Printf("send %v", msg)
+	return c.sendMessage(msg)
+}
+
+// On add the callback for the event
+func (c *Connector) On(event string, callback Callback) {
+	c.muEvents.Lock()
+	defer c.muEvents.Unlock()
+
+	c.events[event] = callback
+}
+
+// Close close the connection, and shutdown the benchmark
+func (c *Connector) Close() {
+	defer func() {
+		// ignore panic
+		recover()
+	}()
+	c.heartTicker.Stop()
+	close(c.die)
+	c.conn.Close()
+}
+
+func (c *Connector) eventHandler(event string) (Callback, bool) {
+	c.muEvents.RLock()
+	defer c.muEvents.RUnlock()
+
+	cb, ok := c.events[event]
+	return cb, ok
+}
+
+func (c *Connector) responseHandler(mid uint) (Callback, bool) {
+	c.muResponses.RLock()
+	defer c.muResponses.RUnlock()
+
+	cb, ok := c.responses[mid]
+	return cb, ok
+}
+
+func (c *Connector) setResponseHandler(mid uint, cb Callback) {
+	c.muResponses.Lock()
+	defer c.muResponses.Unlock()
+
+	if cb == nil {
+		delete(c.responses, mid)
+	} else {
+		c.responses[mid] = cb
+	}
+}
+
+func (c *Connector) sendMessage(msg *message.Message) error {
+	data, err := msg.Encode()
+	if err != nil {
+		return err
+	}
+
+	//log.Printf("%+v",msg)
+
+	payload, err := codec.Encode(packet.Data, data)
+	if err != nil {
+		return err
+	}
+
+	c.mid++
+	c.send(payload)
+
+	return nil
+}
+
+func (c *Connector) write() {
+	defer close(c.chSend)
+
+	for {
+		select {
+		case data := <-c.chSend:
+			// log.Printf("send: %#v\n", data)
+			if err := c.conn.WriteMessage(websocket.BinaryMessage, data); err != nil {
+				log.Println(err.Error())
+				c.Close()
+			}
+
+		case <-c.die:
+			return
+		}
+	}
+}
+
+func (c *Connector) send(data []byte) {
+	c.chSend <- data
+}
+
+func (c *Connector) read() {
+	for {
+		_, buf, err := c.conn.ReadMessage()
+		if err != nil {
+			// log.Println(err.Error())
+			c.Close()
+			return
+		}
+
+		packets, err := c.codec.Decode(buf)
+		if err != nil {
+			log.Println(err.Error())
+			c.Close()
+			return
+		}
+
+		for i := range packets {
+			p := packets[i]
+			c.processPacket(p)
+		}
+	}
+}
+
+func (c *Connector) processPacket(p *packet.Packet) {
+	// fmt.Printf("packet: %#v\n", p)
+	switch p.Type {
+	case packet.Handshake:
+		c.send(had)
+		var data map[string]interface{}
+		if err := json.Unmarshal(p.Data, &data); err != nil {
+			panic(err)
+		}
+		if data[`code`].(float64) != 200 {
+			panic(`code not 200`)
+		}
+
+		sys, _ := data[`sys`].(map[string]interface{})
+		i := time.Duration(sys[`heartbeat`].(float64))
+		c.heartTicker = time.NewTicker(i * time.Second)
+		go func() {
+			for {
+				<-c.heartTicker.C
+				c.send(hbd)
+			}
+		}()
+
+		c.connectedCallback()
+	case packet.Data:
+		msg, err := message.Decode(p.Data)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+		c.processMessage(msg)
+
+	case packet.Kick:
+		c.Close()
+	}
+}
+
+func (c *Connector) processMessage(msg *message.Message) {
+	switch msg.Type {
+	case message.Push:
+		// log.Printf("on %v", msg)
+		cb, ok := c.eventHandler(msg.Route)
+		if !ok {
+			// log.Println("event handler not found", msg.Route)
+			return
+		}
+
+		cb(msg.Data)
+
+	case message.Response:
+		cb, ok := c.responseHandler(msg.ID)
+		if !ok {
+			// log.Println("response handler not found", msg.ID)
+			return
+		}
+
+		cb(msg.Data)
+		c.setResponseHandler(msg.ID, nil)
+	}
+}
+
+func serialize(v proto.Message) ([]byte, error) {
+	data, err := proto.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
